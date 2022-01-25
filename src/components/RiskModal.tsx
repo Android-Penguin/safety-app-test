d0ef21c5fe2b01d260ed85bdda4f65292d57b754
Android-Penguin <49669409+Android-Penguin@users.noreply.github.com>
2022-01-25 22:23:00 +1300

    Gradient slider
    - Added vertical range input to modal.
    
    - Added image for slider thumb.
    
    - Moved slider styling to separate file.
    
    - Added functional styling to gradient of slider.

--- src/components/RiskModal.tsx
+++ src/components/RiskModal.tsx
@@ -2,10 +2,40 @@ import { IonButton, IonButtons, IonContent, IonFooter, IonHeader, IonIcon, IonMo
 import { closeCircle } from 'ionicons/icons';
 import { Dispatch, SetStateAction, useState } from 'react';
 import './RiskModal.css';
+import './SliderStyles.css'
 
 interface RiskModalProps {modalState:boolean, setModalState:Dispatch<SetStateAction<boolean>>}
 
 const RiskModal: React.FC<RiskModalProps> = ({modalState, setModalState}) => {
+    const root = document.querySelector(":root") as HTMLElement;
+    var percentageValue = "55%";
+    
+    function changeValue(slider: any) {
+        var value = Math.round(slider.target.value);
+
+        console.log(value);
+
+        switch(value) {
+            case 1:
+                percentageValue = "10%";
+                break;
+            case 2:
+                percentageValue = "32%";
+                break;
+            case 3:
+                percentageValue = "55%";
+                break;
+            case 4:
+                percentageValue = "76%";
+                break;
+            case 5:
+                percentageValue = "100%";
+                break;
+        }
+
+        // console.log(percentageValue);
+        root.style.setProperty("--sliderPercentage", percentageValue);
+    }
 
     return (
         <IonModal isOpen={modalState} class="risk-modal" onDidDismiss={()=>setModalState(false)}>
@@ -26,7 +56,7 @@ const RiskModal: React.FC<RiskModalProps> = ({modalState, setModalState}) => {
                 <div className="risk-question">What is the consequence if something goes wrong?</div>
                 <div className="selection-ui-container">
                     <div>
-                        <input className="vertical-slider" type="range" min="1" max="5"></input>
+                        <input className="vertical-slider" onInput={changeValue} type="range" min="1" max="5"></input>
                     </div>
                     <div>
 
