d2b24013611e1add6ee48b02c2dc1b9b589e026f
Android-Penguin <49669409+Android-Penguin@users.noreply.github.com>
2022-06-29 15:18:58 +1200

    Fullscreen modal
    - TA modal now always displays fullscreen.
    
    - Added TA heading to modal.

--- src/pages/StackedIcons.tsx
+++ src/pages/StackedIcons.tsx
@@ -17,13 +17,20 @@ const StackedIcons: React.FC<StackedIconProps> = () => {
                     <h1>Toolbox talk</h1>
                     <IonButton id="open-modal">Go to task analysis</IonButton>
                 </div>
-                <IonModal trigger="open-modal">
-                    <IonHeader>
-                        <NavBar href="/stacked-icons" stackedIcon="speech_bubble.svg" />
-                    </IonHeader>
-                    <IonHeader id="return-header">
-                        <a href="/stacked-icons">&lt; Return to task analysis</a>
-                    </IonHeader>
+                <IonModal id="stacked-modal" trigger="open-modal">
+                    <div>
+                        <IonHeader>
+                            <NavBar href="/stacked-icons" stackedIcon="speech_bubble.svg" />
+                        </IonHeader>
+                        <IonHeader id="return-header">
+                            <a href="/stacked-icons">&lt; Return to Toolbox Talk</a>
+                        </IonHeader>
+                        <IonContent>
+                            <div style={{"margin":"10px"}}>
+                                <h1>Task analysis</h1>
+                            </div>
+                        </IonContent>
+                    </div>
                 </IonModal>
             </IonContent>
         </IonPage>
